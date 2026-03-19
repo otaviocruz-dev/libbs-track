ab526b092235c3469a2bd962e9a7b8e3e97f6d23
otaviocruz-dev <ohmcruz07@gmail.com>
2026-03-19 18:12:18 -0300

    Começando estrutura do projeto LibbsTrack
            - Criação dos packages: model.produto, model.estoque, model.logistica, model.regulatorio, model.ia, service e app
            - Implementação de Medicamento, ControleDePrazo e RelatorioRegulatorio com atributos.
            - Todas as classes adicionadas!

---
+++ src/com/libbs/track/app/Principal.java
@@ -0,0 +1,7 @@
+package com.libbs.track.app;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Começo do Libbs Track!");
+    }
+}
---
+++ src/com/libbs/track/modelo/produto/Medicamento.java
@@ -0,0 +1,50 @@
+package com.libbs.track.modelo.produto;
+
+public class Medicamento {
+
+    private String nomePrincipio;
+    private String nomeComercial;
+    private String concentracao;
+    private String formaFarmaceutica;
+    private String numRegistroAnvisa;
+
+    public String getNomePrincipio() {
+        return nomePrincipio;
+    }
+
+    public void setNomePrincipio(String nomePrincipio) {
+        this.nomePrincipio = nomePrincipio;
+    }
+
+    public String getNomeComercial() {
+        return nomeComercial;
+    }
+
+    public void setNomeComercial(String nomeComercial) {
+        this.nomeComercial = nomeComercial;
+    }
+
+    public String getConcentracao() {
+        return concentracao;
+    }
+
+    public void setConcentracao(String concentracao) {
+        this.concentracao = concentracao;
+    }
+
+    public String getFormaFarmaceutica() {
+        return formaFarmaceutica;
+    }
+
+    public void setFormaFarmaceutica(String formaFarmaceutica) {
+        this.formaFarmaceutica = formaFarmaceutica;
+    }
+
+    public String getNumRegistroAnvisa() {
+        return numRegistroAnvisa;
+    }
+
+    public void setNumRegistroAnvisa(String numRegistroAnvisa) {
+        this.numRegistroAnvisa = numRegistroAnvisa;
+    }
+}
